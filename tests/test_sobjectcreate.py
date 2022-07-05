acb640c164c3005383e52ec83654956d54d31979
Sai Praneeth M <saipraneeth@aarth.io>
2022-07-05 15:51:25 +0530

    basic credential test case in create object

---
+++ tests/test_sobjectcreate.py
@@ -0,0 +1,10 @@
+import pytest
+from cmem_plugin_salesforce.workflow.soql_query import SoqlQuery
+from simple_salesforce.exceptions import SalesforceAuthenticationFailed
+
+
+def test_invalid_credentials():
+    """Test plugin execution"""
+    with pytest.raises(SalesforceAuthenticationFailed):
+        SoqlQuery(username='', password='', security_token='', dataset='', soql_query='SELECT Id, Name FROM Contact')
+
--- tests/test_soql.py
+++ tests/test_soql.py
@@ -1,10 +1,10 @@
 """Plugin tests."""
 import pytest
-from cmem_plugin_salesforce.workflow.soql_query import SoqlQuery
+from cmem_plugin_salesforce.workflow.operations import SobjectCreate
 from simple_salesforce.exceptions import SalesforceAuthenticationFailed
 
 
 def test_invalid_credentials():
     """Test plugin execution"""
     with pytest.raises(SalesforceAuthenticationFailed):
-        SoqlQuery(username='', password='', security_token='', dataset='', soql_query='SELECT Id, Name FROM Contact')
+        SobjectCreate(username='', password='', security_token='', salesforce_object="Lead")
